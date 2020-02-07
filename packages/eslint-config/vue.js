d7ff8c00ad2d2ca38edaf6f743e2e219cc537fc6
JeanCHhhh <jin.chen@shanbay.com>
2020-02-07 15:55:00 +0800

    feat: add vue rules

--- packages/eslint-config/vue.js
+++ packages/eslint-config/vue.js
@@ -1,9 +1,16 @@
 module.exports = {
-  extends: [require.resolve('./base'), 'plugin:vue/recommended'],
+  extends: [
+    require.resolve('./base'),
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended',
+    'prettier/vue',
+  ],
   rules: {
+    // import
+    'import/extensions': 0,
     'import/no-unresolved': 0,
-    'vue/html-self-closing': 0,
-    'vue/max-attributes-per-line': 0,
+    // vue
+    'vue/html-indent': [1, 4],
   },
   globals: {
     Vue: true,
